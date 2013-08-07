3ebf7ae760097df41158aaa9a6211a2fea66f0bc
Jacob Walker <jacob.walker94+github@gmail.com>
2013-08-07 17:24:39 +0100

    Added support for a $usesRepositories property in controllers

--- Core/Controller/Controller.php
+++ Core/Controller/Controller.php
@@ -5,6 +5,17 @@ class Controller
     {
         $this->data = $data;
         $this->query = $query;
+
+        if (is_array($this->usesRepositories))
+        {
+            foreach ($this->usesRepositories as $useRepository)
+            {
+                $repositoryName = ucfirst(strtolower($useRepository)) . 'Repository';
+                require 'Application/Repository/' . $repositoryName . '.php';
+
+                $this->$repositoryName = new $repositoryName();
+            }
+        }
     }
 
     public function Go($action)
