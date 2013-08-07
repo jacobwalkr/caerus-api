0f675d100a44fe49b56b62c83dc5ae5e0552343b
Jacob Walker <jacob.walker94+github@gmail.com>
2013-08-07 17:23:57 +0100

    Fleshed out System.php and included properly in index.php

--- Core/System.php
+++ Core/System.php
@@ -1,9 +1,49 @@
 <?php
-static class System
+class System
 {
+    public static $Database;
+    private static $config;
+
+    public static function Initialise()
+    {
+        self::loadConfig();
+        self::$Database = self::loadDatabaseConnector();
+    }
+
     public static function LoadService($service)
     {
         require 'Application/Model/' . $service . '.php';
         return new $service();
+    }
+
+    private static function loadConfig()
+    {
+        self::$config = array();
+
+        // Only database config to consider now
+        self::$config['database'] = json_decode(file_get_contents('Application/Config/database.json'));
+    }
+
+    private static function loadDatabaseConnector()
+    {
+        $dbConfig = self::$config['database'];
+        $connector = $dbConfig->connector;
+        $dbn = $dbConfig->database;
+        $host = $dbConfig->host;
+        $user = $dbConfig->user;
+        $password = $dbConfig->password;
+
+        $dsn = $connector . ':dbname=' . $dbn . ';host=' . $host;
+
+        try
+        {
+            $pdo = new PDO($dsn, $user, $password);
+        }
+        catch (PDOException $exception)
+        {
+            error_log($exception->getMessage());
+        }
+
+        return $pdo;
     }
 }
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+foreach (glob('Core/*.php') as $file)
+{
+    require $file;
+}
+
 foreach (glob('Core/*/') as $folder)
 {
     foreach (glob($folder . '*.php') as $file)
