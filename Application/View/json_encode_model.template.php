90e88a38dcaad1faf24833bdb59c09d82aea0f6e
Jacob Walker <jacob.walker94@gmail.com>
2013-08-10 12:03:02 +0400

    Put pretty JSON constants back

--- Application/View/json_encode_model.template.php
+++ Application/View/json_encode_model.template.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/javascript');
 $callback = $this->parameters['callback'];
-$json = json_encode($this->model);
+$json = json_encode($this->model, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
 
 if (!is_null($callback))
 {
